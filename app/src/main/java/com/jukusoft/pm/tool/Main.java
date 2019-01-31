f0e074b706a514c62a5fb5c06aa77c3b85f802ad
juku <kuenzel.justin@t-online.de>
2019-01-31 18:42:10 +0100

    Changed the scan base package for spring to search for beans and so on

--- app/src/main/java/com/jukusoft/pm/tool/Main.java
+++ app/src/main/java/com/jukusoft/pm/tool/Main.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication(scanBasePackages = "com.jukusoft")
+@SpringBootApplication(scanBasePackages = "com.jukusoft.pm.tool")
 @RestController
 public class Main {
 
