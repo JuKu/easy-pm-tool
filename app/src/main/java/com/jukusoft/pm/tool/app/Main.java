54b1132467b43d3405b23c537c8715948730fa7a
juku <kuenzel.justin@t-online.de>
2019-02-03 03:35:10 +0100

    Improved software quality for sonarqube

--- app/src/main/java/com/jukusoft/pm/tool/app/Main.java
+++ app/src/main/java/com/jukusoft/pm/tool/app/Main.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.RestController;
 
