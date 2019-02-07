28732d1f4292c334bc56e867e6491a0736742e49
juku <kuenzel.justin@t-online.de>
2019-02-07 22:34:18 +0100

    Fix UserDAO

--- module-def/src/main/java/com/jukusoft/pm/tool/def/dao/UserDAO.java
+++ module-def/src/main/java/com/jukusoft/pm/tool/def/dao/UserDAO.java
@@ -13,26 +13,7 @@ import java.util.Optional;
 @Repository
 public interface UserDAO extends PagingAndSortingRepository<User, Integer> {
 
-    @Override
-    public User save(User user);
-
-    @Cacheable("user-findAll")
-    @Override
-    public List<User> findAll();
-
-    /**
-     * Retrieves an entity by its id.
-     *
-     * @param userID must not be {@literal null}.
-     * @return the entity with the given id or {@literal Optional#empty()} if none found
-     * @throws IllegalArgumentException if {@code id} is {@literal null}.
-     */
-    Optional<User> findById(int userID);
-
-    @Query("FROM com.jukusoft.pm.tool.def.model.User u where u.username = :name")
+    @Query("SELECT u FROM com.jukusoft.pm.tool.def.model.User u where u.username = :name")
     Optional<User> findByUsername(@Param("name") String username);
-
-    @Override
-    public long count();
 
 }
